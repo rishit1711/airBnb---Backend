e196a96d5e01472175bedbdda10546147398bfba
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-09 12:07:41 +0530

    Auth Related dto and Service Implementation

--- airBnbApp/airBnbApp/src/main/java/com/example/Project_Rishit/airBnbApp/repository/UserRepository.java
+++ airBnbApp/airBnbApp/src/main/java/com/example/Project_Rishit/airBnbApp/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.Project_Rishit.airBnbApp.repository;
 import com.example.Project_Rishit.airBnbApp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
 
 }
