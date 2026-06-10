0df2cb24290fe17b89d32828758296676dca8eeb
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-11 01:15:17 +0530

    Authenitcation and Authorization Done

---
+++ airBnbApp/airBnbApp/src/main/java/com/example/Project_Rishit/airBnbApp/Exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.example.Project_Rishit.airBnbApp.Exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
